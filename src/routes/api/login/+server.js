207bac61c4ca36d8926a83e37e497858c4f3d8c6
jarren <thirdwilliam1@gmail.com>
2025-10-21 16:38:41 +0700

    fix conflict

--- src/routes/api/login/+server.js
+++ src/routes/api/login/+server.js
@@ -1,23 +1,4 @@
 import { json } from "@sveltejs/kit";
-<<<<<<< HEAD
-import { pool } from "$lib/server/database";
-import bcrypt from "bcryptjs"
-import { generateToken } from "$lib/jwt/jwt";
-export async function POST({request, cookies}) {
-    try{
-        const {email, password} = await request.json()
-        if (!email || !password){
-            return json(
-                {
-                    success : false,
-                    message : "Email and password required"
-                },
-                {
-                    status : 400
-                }
-            )
-        }
-=======
 import { pool } from "$lib/server/database.js";
 import { generateToken } from "$lib/jwt/jwt.js";
 import bcrypt from "bcryptjs";
@@ -40,19 +21,10 @@ export async function POST({request, cookies}) {
         
         }
 
->>>>>>> 57ed4c61591ef4424b8f75cd8c983b8910547664
         const [users] = await pool.execute(
             "SELECT * FROM user WHERE email = ?",
             [email]
         )
-<<<<<<< HEAD
-        const user = users[0]
-        if (!user) {
-            return json(
-                {
-                    success : false,
-                    message : "Incorrect email"
-=======
 
         const user = users[0]
         if(!user){
@@ -60,62 +32,12 @@ export async function POST({request, cookies}) {
                 {
                     success : false,
                     message : 'Incorrect email!'
->>>>>>> 57ed4c61591ef4424b8f75cd8c983b8910547664
                 },
                 {
                     status : 401
                 }
             )
         }
-<<<<<<< HEAD
-        const isValidPassword = await bcrypt.compare(password, user.password)
-        if (!isValidPassword) {
-            return json(
-                {
-                    success : false,
-                    message : "Incorrect password"
-                },
-                {
-                    status : 401
-                }
-            )
-        }
-        const token = generateToken({
-            userId : user.id,
-            email : user.email,
-            username : user.username
-        })
-        cookies.set('token', token, {
-            httpOnly : true,
-            sameSite : 'strict',
-            maxAge : 60 * 60 * 24 * 7,
-            path : '/'
-        })
-        return json(
-            {
-                success : true,
-                message : "Login successful",
-                data : {
-                    token,
-                    user : {
-                        id : user.id,
-                        username : user.username,
-                        email : user.email
-                    }
-                }
-            }
-        )
-    } catch(err) {
-        console.error(err);
-        return json(
-            {
-                success : false,
-                message : "Error"
-            },
-            {
-                status : 500
-            }
-=======
         // Password validation
         const isValidPassword = await bcrypt.compare(password,user.password)
         if(!isValidPassword){
@@ -159,7 +81,6 @@ export async function POST({request, cookies}) {
         return json(
             { success : false, message : 'Something went wrong'},
             { status : 500}
->>>>>>> 57ed4c61591ef4424b8f75cd8c983b8910547664
         )
     }
 }
