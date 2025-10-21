093769a9cb7fc82ef7c0a3f126d71648641df78c
jarren <thirdwilliam1@gmail.com>
2025-10-21 16:40:16 +0700

    fix conflict

--- src/routes/api/register/+server.js
+++ src/routes/api/register/+server.js
@@ -1,28 +1,5 @@
 import { json } from "@sveltejs/kit";
 import { pool } from "$lib/server/database";
-<<<<<<< HEAD
-import bcrypt from "bcryptjs"
-export async function POST({request}){
-    try{
-        const { username, email, password } = await request.json()
-        const hashedPassword = await bcrypt.hash(password, 12)
-        const [result] = await pool.execute('INSERT INTO user (username, email, password, created_at) VALUES (? ,? ,? ,NOW())', [username, email, hashedPassword])
-        console.log(result);
-        if (result){
-            return json({
-                success : true,
-                message : "Register successful",
-                data : {
-                    id : result.insertId
-                }
-            },
-            {
-                status : 200
-            })
-        }
-    } catch(err) {
-        console.log(err);
-=======
 import bcrypt from "bcryptjs";
 
 
@@ -47,6 +24,5 @@ export async function POST({request}){
         
     }catch(e){
         console.error(e)
->>>>>>> 57ed4c61591ef4424b8f75cd8c983b8910547664
     }
 }
